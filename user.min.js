967e3db370521f82c4efd13771d2d548dbee304c
Rack Lin <racklin@gmail.com>
2013-08-20 16:54:13 +0800

    Added match meta tags Signed-off-by: Rack Lin <racklin@gmail.com>

--- user.min.js
+++ user.min.js
@@ -5,6 +5,8 @@
 // @author         racklin@gmail.com
 // @include     https://www.facebook.com/*
 // @include     http://www.facebook.com/*
+// @match     https://www.facebook.com/*
+// @match     http://www.facebook.com/*
 // @run-at         document-end
 // @require        http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.9.1.min.js
 // @version     1.1
